b3974fcf7888b5dd29a73a84f8270d3f90424b1b
Andrei Neculaesei <neculaesei@users.noreply.github.com>
2019-07-16 12:06:35 +0300

    prefer global eslint

--- src/utils.ts
+++ src/utils.ts
@@ -1,28 +1,27 @@
-import path from 'path'
-import fs from 'fs'
-import which from 'which'
+import path from "path";
+import fs from "fs";
+import which from "which";
 
 function exists(file: string): boolean {
-  return fs.existsSync(file)
+  return fs.existsSync(file);
 }
 
 export async function findEslint(rootPath: string): Promise<string> {
-  const platform = process.platform
-  if (
-    platform === 'win32' &&
-    (exists(path.join(rootPath, 'node_modules', '.bin', 'eslint.cmd')))
-  ) {
-    return path.join('.', 'node_modules', '.bin', 'eslint.cmd')
-  } else if (
-    (platform === 'linux' || platform === 'darwin') &&
-    (exists(path.join(rootPath, 'node_modules', '.bin', 'eslint')))
-  ) {
-    return path.join('.', 'node_modules', '.bin', 'eslint')
-  } else {
-    try {
-      return which.sync('eslint')
-    } catch (e) {
-      return ''
+  const platform = process.platform;
+  try {
+    return which.sync("eslint");
+  } catch (error) {
+    if (
+      platform === "win32" &&
+      exists(path.join(rootPath, "node_modules", ".bin", "eslint.cmd"))
+    ) {
+      return path.join(".", "node_modules", ".bin", "eslint.cmd");
+    } else if (
+      (platform === "linux" || platform === "darwin") &&
+      exists(path.join(rootPath, "node_modules", ".bin", "eslint"))
+    ) {
+      return path.join(".", "node_modules", ".bin", "eslint");
     }
+    return "";
   }
 }
